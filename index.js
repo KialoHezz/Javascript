02708dcfb9b274acae56c27677a7622be9a97417
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-04-19 14:27:39 +0300

    Mini-weather application[conditional operators(==, ===, !=, e.t.c)]

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 // console.log("Hezron Kyalo");
 
 // User Inputs 1. Prompt is a js built-in js functionality that helps get inputs from a user through the broswer.
-// const prompt=require("prompt-sync")({sigint:true});
+const prompt=require("prompt-sync")({sigint:true});
 
 // fruit = prompt('What is your favorite fruit ?');
 
@@ -22,7 +22,6 @@
 // console.log("tip :" + tipAmount);
 // console.log("total is :" + total);
 
-console.log(Math.round()*3);
 
 // functions
 
@@ -34,9 +33,35 @@ console.log(Math.round()*3);
 /* datatype : int, string, boolean, decimal, array [] , objects,
  Build a Weather App */
  
-//  Math operators Exponents**, modulo/reminder% , subtractio+ , _
+ //  Math operators Exponents**, modulo/reminder% , subtractio+ , _
+ 
+ // Math.floor() method rounds a number down to the nearest integer and return the result.
+ // math.ceil() method rounds a number to the nearest integer and returns the result.
+ //  math.round() return a random number between 0(inclusive), and 1(exclusive)
+ // 
+ 
+ 
+ /* let & var the value can be change while const value cannot be changed.
+  */
 
-// Math.floor() method rounds a number down to the nearest integer and return the result.
-// math.ceil() method rounds a number to the nearest integer and returns the result.
-//  math.round() return a random number between 0(inclusive), and 1(exclusive)
-// 
+//  Baby wether app => conditionals
+
+//  if rain the grab your umbrella
+// else wear your sunglassess
+// conditionals areuused in statements to compare variable values and data types. and they always return a boolean - true / false
+//  == (double equal) - checks for equality
+// e.g x == 8
+//  === (triple equal sign) - checks for equality and data types
+//  >, < - greater than , less than
+// <= >= - greate than or equal to and less than or equal to
+// != not or equal to
+// !== not equal value or type
+let Weather = prompt("Enter weather in your region: ")
+
+if (Weather == 'rain'){
+    console.log('grab your umbrella');
+}else{
+    console.log('wear your sunglassess');
+}
+
+
