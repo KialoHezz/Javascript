3849653562762592ee9ac24ead7c2f2c9c3aef8b
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-04-19 10:21:41 +0300

    Define prompt to avoid the error[Reference Error(prompt is not dfn)]

---
+++ index.js
@@ -0,0 +1,12 @@
+// comments
+// operators [*,+,-,%]
+// variable
+
+// console.log("Hezron Kyalo");
+
+// User Inputs 1. Prompt is a js built-in js functionality that helps get inputs from a user through the broswer.
+const prompt=require("prompt-sync")({sigint:true});
+
+fruit = prompt('What is your favorite fruit ?');
+
+console.log(fruit);
