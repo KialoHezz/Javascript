d639fd5c51de2ffa2520eb79628940a8f5094988
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-04-19 13:58:08 +0300

    Operator,methods

--- index.js
+++ index.js
@@ -5,22 +5,38 @@
 // console.log("Hezron Kyalo");
 
 // User Inputs 1. Prompt is a js built-in js functionality that helps get inputs from a user through the broswer.
-const prompt=require("prompt-sync")({sigint:true});
+// const prompt=require("prompt-sync")({sigint:true});
 
-fruit = prompt('What is your favorite fruit ?');
+// fruit = prompt('What is your favorite fruit ?');
 
-console.log(fruit);
+// console.log(fruit);
 
-// datatype : int, string, boolean
 
-food = prompt('How Much wa the food:');
+// food = prompt('How Much wa the food:');
 
-tipPercentage = prompt('tip %:') / 100;
+// tipPercentage = prompt('tip %:') / 100;
 
-tipAmount = food * tipPercentage;
-total = food + tipPercentage;
+// tipAmount = food * tipPercentage;
+// total = food + tipPercentage;
 
-console.log("tip :" + tipAmount);
-console.log("total is :" + total);
+// console.log("tip :" + tipAmount);
+// console.log("total is :" + total);
+
+console.log(Math.round()*3);
 
 // functions
+
+
+// type casting
+
+
+// Lesson 2 :
+/* datatype : int, string, boolean, decimal, array [] , objects,
+ Build a Weather App */
+ 
+//  Math operators Exponents**, modulo/reminder% , subtractio+ , _
+
+// Math.floor() method rounds a number down to the nearest integer and return the result.
+// math.ceil() method rounds a number to the nearest integer and returns the result.
+//  math.round() return a random number between 0(inclusive), and 1(exclusive)
+// 
