d91b9eb3e70641e183fe6603a689c1782e6eeb42
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-04-19 13:33:51 +0300

    Lesson 1

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ const prompt=require("prompt-sync")({sigint:true});
 fruit = prompt('What is your favorite fruit ?');
 
 console.log(fruit);
+
+// datatype : int, string, boolean
+
+food = prompt('How Much wa the food:');
+
+tipPercentage = prompt('tip %:') / 100;
+
+tipAmount = food * tipPercentage;
+total = food + tipPercentage;
+
+console.log("tip :" + tipAmount);
+console.log("total is :" + total);
+
+// functions
