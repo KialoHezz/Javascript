ae7d93ba86db326760db1d58693c6174402c78be
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-04-25 08:49:43 +0300

    If else statement

--- index.js
+++ index.js
@@ -6,4 +6,24 @@ const randomSelection = (fruits) => {
 }
 // Arrays
 fruits = ["Apple", "Mango", "Orange", "Banana", "quavas"];
-console.log(randomSelection(fruits));
+// console.log(randomSelection(fruits));
+
+
+//  rainy (1), sunny (-1), overcast (0)
+const weatherScorer = (weather, weather2) => {
+  let score;
+
+  if( weather == 'rainy' && weather2 == 'overcast'){
+    score = 2;
+  } else if( weather == 'rainy'){
+    score = 1
+  } else if (weather == 'sunny') {
+    score = -1
+  }else {
+    score = 0
+  }
+
+  return score
+}
+
+console.log(weatherScorer('rainy'));
