1dd534bb5c098de771e55351bfe2f080a1d4d425
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-04-25 16:53:17 +0300

    Search hero BY: 1. id 2. name

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ import fetch from "node-fetch";
 
 const SUPERHERO = '1415420709270129';
 const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO}`
-let getNewHeroDiv = document.getElementById("getNewHero");
-let heroImageDiv = document.getElementById("heroImage");
+const getNewHeroDiv = document.getElementById("getNewHero");
+const heroImageDiv = document.getElementById("heroImage");
+const searchBtn = document.getElementById("searchBtn");
+
+const searchInput = document.getElementById("searchInput")
+
 
 const getSuperHero = (id, name) => {
+  // Someone searches : 1. By name => base_url/search/batman
+  // json.result[0].image.url
+  
+  // 2. id => base_url/id
+  // json.image.url
   fetch(`${BASE_URL}/${id}`)
     .then(response => response.json())
     .then(json =>{
@@ -15,6 +24,16 @@ const getSuperHero = (id, name) => {
   
 }
 
+const getSearchSuperHero = (name) => {
+  fetch(`${BASE_URL}/search/${name}`) 
+    .then(response => response.json())
+    .then(json =>{
+      const hero = json.results[0]
+      console.log(hero )
+      heroImageDiv.innerHTML = `<imag src='${hero.image.url}' height=200 width=200>`
+  })
+}
+
 const randomHero = () =>  {
   const numberOfHeroes = 731
 
@@ -22,4 +41,5 @@ const randomHero = () =>  {
 }
 
 getNewHeroDiv.onclick = () => getSuperHero(randomHero );
+searchBtn.onclick = () => getSearchSuperHero(searchInput.value)
 
