dbd069d324f425acf302bf38e50be225d0e0da75
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-04-26 11:54:40 +0300

    [200~Understood now, running in browser and in vscode is 2 different things. What works in the browser won't necessarily work in Node.js When running in browser, there's no need to import fetch.~

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import fetch from "node-fetch";
+// import fetch from "/node-fetch";
 
 const SUPERHERO = '1415420709270129';
 const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO}`
+console.log(BASE_URL)
+
 const getNewHeroDiv = document.getElementById("getNewHero");
 const heroImageDiv = document.getElementById("heroImage");
 const searchBtn = document.getElementById("searchBtn");
@@ -25,32 +27,29 @@ const getSuperHero = (id, name) => {
 }
 
 const getSearchSuperHero = (name) => {
+  console.log(searchInput.value);
   fetch(`${BASE_URL}/search/${name}`) 
     .then(response => response.json())
     .then(json =>{
-      const hero = json.results[0]
-      // console.log(hero )
-      const superHero = json
-      showHeroInfo(superHero)
+      const hero = json.results[0] 
+      showHeroInfo(hero)
      
   })
 }
 
 
 const showHeroInfo = (character) => {
-  const name = `<h2>${character.name}</h2>`
-  const img = `<imag src='${hero.image.url}' height=200 width=200>`
+  const name = `<h2>${character.name}</h2>`;
+  const img = `<imag src="${character.image.url}" height=200 width=200>`;
 
   const stats = Object.keys(character.powerstats).map(stat => {
-    `<p>${stat}: ${character.powerstats[stat.toUpperCase()]}</p>`
+    return `<p>${stat}: ${character.powerstats[stat.toUpperCase()]}</p>`
   }).join('')
 
   // return an array of p tags
-  console.log(stats.join(""));
+  // console.log(stats.join(""));
 
   heroImageDiv.innerHTML = `${name} ${img} ${stats}`
-
-  return stats.join('')
 } 
 
 
