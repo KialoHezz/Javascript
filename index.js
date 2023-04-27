50a7cd5a54da851beac76f72b37d200508429c44
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-04-27 17:38:09 +0300

    Bank Application Using classes and DOM

--- index.js
+++ index.js
@@ -1,45 +1,100 @@
-// OOP[OBJECT ORIENTED PROGRAMMINNG]
-// class
-// methods
-// 
-// properties
+// // OOP[OBJECT ORIENTED PROGRAMMINNG]
+// // class
+// // methods
+// // 
+// // properties
  
-// this
-class Car {
-    constructor(name, color, speed){
-        this.name = name
-        this.color = color
-        this.speed = speed
-    }
+// // this
+// class Car {
+//     constructor(name, color, speed){
+//         this.name = name
+//         this.color = color
+//         this.speed = speed
+//     }
 
-    drive(){
-        console.log("Just drove 2 mile");
-    }
+//     drive(){
+//         console.log("Just drove 2 mile");
+//     }
 
-    brake(){
-        console.log("Just hit the brake");
+//     brake(){
+//         console.log("Just hit the brake");
+//     }
+// }
+
+// const porsche = new Car('Porsche','yellow', 250)
+// // console.log(porsche.name);
+// // console.log(porsche.color);
+// // console.log(porsche.speed);
+
+// // instantiate
+// // const nums = [1, 2, 3, 4, 5]
+// // nums.forEach(_ => porsche.drive())
+
+
+// // add method into array datatype
+
+// Array.prototype.myPush = function(item){
+//     this[this.length] = item
+//     return this
+// }
+
+
+// const fruits = ["Banana", "Apple", "Mango"]
+
+
+// console.log(fruits.myPush("nnana"));
+
+
+
+class Bank {
+    constructor(balanced){
+        this.balanced = balanced
+    }
+    withdraw(amount) {
+        if (this.balanced- amount <= 0){
+            console.log('Cannot');
+            return
+        }
+        this.balanced -= amount
+        console.log('Withdrawal', `Ksh ${amount}`);
+        console.log({balance: this.balanced});
+    }
+    deposit(amount){
+        this.balanced += amount
+        console.log('deposit', `Ksh ${amount}`);
+        console.log({balance: this.balanced});
+
     }
 }
 
-const porsche = new Car('Porsche','yellow', 250)
-// console.log(porsche.name);
-// console.log(porsche.color);
-// console.log(porsche.speed);
 
-// instantiate
-// const nums = [1, 2, 3, 4, 5]
-// nums.forEach(_ => porsche.drive())
+const hezronChecking = new Bank(100)
 
 
-// add method into array datatype
 
-Array.prototype.myPush = function(item){
-    this[this.length] = item
-    return this
+// hezronChecking.deposit(200)
+hezronChecking.withdraw(200)
+
+// get the div by Id
+ 
+const depositBtn = document.getElementById("deposit");
+const withdrawBtn = document.getElementById("withdraw");
+const balanceDiv = document.getElementById("balance");
+const amountInput = document.getElementById("amount");
+
+depositBtn.onclick = () => {
+    const amount = Number(amountInput.value)
+    // console.log(amount);
+    hezronChecking.deposit(amount)
+
+    balanceDiv.innerText = `Balance: ${hezronChecking.balanced}`
+
 }
 
+withdrawBtn.onclick = () => {
+    const amount = Number(amountInput.value)
+    hezronChecking.withdraw(amount)
 
-const fruits = ["Banana", "Apple", "Mango"]
+    balanceDiv.innerText = `Balance: ${hezronChecking.balanced}`
 
-
-console.log(fruits.myPush("nnana"));
+}
