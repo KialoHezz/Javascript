68d2d9d13418aa88b283da40b37f403f38fffe3e
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-04-27 09:40:59 +0300

    Get the Data then call async func and use .then

--- index.js
+++ index.js
@@ -24,87 +24,42 @@ const promise1 = new Promise((resolve, reject) => {
     setTimeout(() => {
         let isReady
 
-        isReady = false;
+        isReady = [true, false][Math.floor(Math.random() * 2)];
         // tenary operator
         isReady ? resolve('Soup is ready'): 
         reject('No soup today');
     }, 2000)
 });
 
-// excer
+// RESOLVED -> if waiter brings you a soup
+// then you tip the waiter
+// and pay for the soup
+
+// REJECT -> LEAVE A BAD review 
+// No tip
 const promise = async() =>{
+    // variables in objects
+    const data = {rating: 0, tip: 0 , pay: 0, review: 0}
+   
     try{
         const soup = await promise1
         console.log(soup);
+        data.rating = 5 + 'Stars'
+        data.tip = .2
+        data.review = 5
+        data.pay = 1000
+
+        return data
     } catch (error){
         console.log(error);
+        data.rating = 0 + 'stars'
+        data.tip = 0
+        data.pay = 0
+        data.review = 1
+
+        return data
     }
 }
 
-promise()
+promise().then(value => console.log(value))
 
-// // console.log(promise1.then(value => console.log(value)));
-
-
-// const promise2 = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         let isReady 
-
-//         isReady = false;
-//         // tenary operator
-//         isReady ? resolve('Soup is ready'): 
-//         reject('No soup today');
-//     }, 2000)
-// });
-
-// // catch the reject
-// // console.log(promise2
-// //                 .then(success => console.log({success}))
-// //                 // below where error happens
-// //                 .catch(Err => console.log({Err}))
-// // )
-
-
-// // index true or false
-// // const promiseTF = new Promise((resolve, reject) => {
-// //     setTimeout(() => {
-// //         let isReady
-// //         // get random true or false
-// //         isReady = [true, false][Math.floor(Math.random() * 2)];
-// //         // tenary operator
-// //         isReady ? resolve('Soup is ready'): 
-// //         reject('No soup today');
-// //     }, 2000)
-// // });
-
-// // console.log(promise2
-// //                 .then(success => console.log({success}))
-// //                 // below where error happens
-// //                 .catch(Err => console.log({Err}))
-// //             )
-
-
-// // console.log('fetch:', 
-// // fetch('https://dog.ceo/api/breeds/image/random')
-// //     // get a response
-// //     .then(response => console.log("response:" ,response.json()))
-// //     .then(data => console.log({data}))
-
-// // );
-
-
-// // async &Await (asynchronous programmming)
-// // RULES to use Async / await
-// // ** 1. You must create a function *
-// //  ** 2. you must use the keyword async
-// // ** 3. use the word await
-
-// const getDOg = async () => {
-//     const url = 'https://dog.ceo/api/breeds/image/random';
-//     // await is the same as .then(response => response.json())
-//     const response = await fetch(url);
-//     const data = await response.json()
-//     // console.log(data);
-// }
-
-// // getDOg()
