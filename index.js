5c3b19844b67b74940425e45a688ad871affb217
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-04-27 15:42:33 +0300

    hard coded Array

--- index.js
+++ index.js
@@ -4,15 +4,42 @@
 // 
 // properties
  
+// this
 class Car {
     constructor(name, color, speed){
         this.name = name
         this.color = color
         this.speed = speed
     }
+
+    drive(){
+        console.log("Just drove 2 mile");
+    }
+
+    brake(){
+        console.log("Just hit the brake");
+    }
 }
 
-const ferrari = new Car('ferrari','red', 250);
+const porsche = new Car('Porsche','yellow', 250)
+// console.log(porsche.name);
+// console.log(porsche.color);
+// console.log(porsche.speed);
+
+// instantiate
+// const nums = [1, 2, 3, 4, 5]
+// nums.forEach(_ => porsche.drive())
 
 
-console.log(ferrari);
+// add method into array datatype
+
+Array.prototype.myPush = function(){
+    this[3] = 'kiwa'
+    return this
+}
+
+
+const fruits = ["Banana", "Apple", "Mango"]
+
+
+console.log(fruits.myPush());
