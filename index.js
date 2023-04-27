54e08b10e4afbe00f833a1a982ec3d2f576dd87d
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-04-27 18:09:35 +0300

    addEventListener[Mouse,Keyboard]

--- index.js
+++ index.js
@@ -7,3 +7,31 @@ while (num < 5){
     console.log("looping", num);
     num += 1
 }
+
+
+// addEventListener  
+const playDiv = document.getElementById("play");
+
+playDiv.addEventListener('click', function(){
+    // console.log("play");
+})
+
+playDiv.onclick = () => {
+    // console.log("Yo");
+}
+
+
+let displayDiv = document.getElementById("display");
+document.addEventListener('keydown', function(e){
+    if (e.key == "q") {
+        console.log("you pressed Q");
+        displayDiv.innerText = "You pressed Q"
+    } else{
+        console.log("Not Q");
+        displayDiv.innerText = "Pressed another key; NOT q"
+    }
+
+})
+
+/
+
